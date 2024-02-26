4f570a8ad07c1989fa092ee06712cb5e81c9f5fd
Alexandre <vensalex1991@gmail.com>
2024-02-26 11:25:18 +0100

    Init project

--- database/providers.ts
+++ database/providers.ts
@@ -1,11 +1,14 @@
+import dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
+
+dotenv.config();
+
+// Your code here
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(
-        'mongodb://admin:admin@ac-9126ujr-shard-00-00.obbvy8l.mongodb.net:27017,ac-9126ujr-shard-00-01.obbvy8l.mongodb.net:27017,ac-9126ujr-shard-00-02.obbvy8l.mongodb.net:27017/?replicaSet=atlas-ne371n-shard-0&ssl=true&authSource=admin',
-      ),
+      mongoose.connect(process.env.MONGO_URI),
   },
 ];
